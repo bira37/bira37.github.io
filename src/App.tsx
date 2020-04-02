58140d8c3d362eb2f92f6f9d9f8d48557ad4baca
Ubiratan Neto <ubiratanneto37@hotmail.com>
2020-04-01 22:24:00 -0300

    add navigation component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ErrorBoundary from "./Components/ErrorBoundary";
 import HomePage from "./Components/HomePage";
+import Navigation from "./Components/Navigation/index";
 
 const App = (): JSX.Element => {
   return (
     <ErrorBoundary>
       <HomePage />
+      <Navigation />
     </ErrorBoundary>
   );
 };
---
+++ src/Components/Navigation/index.tsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import { FaFolder, FaLaptopCode, FaRegCheckSquare } from "react-icons/fa";
+import { IoIosArrowUp } from "react-icons/io";
+import { TiPencil } from "react-icons/ti";
+import s from "./styles.scss";
+
+const Navigation = (): JSX.Element | null => {
+  const [displayElement, setDisplayElement] = useState(window.innerWidth < 1128);
+
+  useEffect(() => {
+    const handleResize = (): void => {
+      console.log(window.innerWidth);
+      setDisplayElement(window.innerWidth < 1128);
+    };
+
+    window.addEventListener("resize", handleResize);
+  });
+
+  return displayElement ? (
+    <div className={s.contentStyle}>
+      <span className={s.linkWrapper}>
+        <a href="#about">
+          <TiPencil className={`${s.iconLink} ${s.linkStyle}`} />
+        </a>
+      </span>
+      <span className={s.linkWrapper}>
+        <a href="#programming-skills">
+          <FaLaptopCode className={`${s.iconLink} ${s.linkStyle}`} />
+        </a>
+      </span>
+      <span className={s.linkWrapper}>
+        <a href="#general-skills">
+          <FaRegCheckSquare className={`${s.iconLink} ${s.linkStyle}`} />
+        </a>
+      </span>
+      <span className={s.linkWrapper}>
+        <a href="#projects">
+          <FaFolder className={`${s.iconLink} ${s.linkStyle}`} />
+        </a>
+      </span>
+      <span className={s.linkWrapper}>
+        <a href="#app">
+          <IoIosArrowUp className={`${s.iconLink} ${s.linkStyle}`} />
+        </a>
+      </span>
+    </div>
+  ) : null;
+};
+
+export default Navigation;
