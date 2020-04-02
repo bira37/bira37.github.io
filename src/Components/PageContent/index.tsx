74d25036796d4d4a971fb38148a8b83beadef3c3
Ubiratan Neto <ubiratanneto37@hotmail.com>
2020-04-01 21:07:04 -0300

    add projects

--- src/Components/PageContent/index.tsx
+++ src/Components/PageContent/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import About from "../About/index";
 import GeneralSkills from "../GeneralSkills/index";
 import ProgrammingSkills from "../ProgrammingSkills/index";
+import Projects from "../Projects/index";
 import s from "./styles.scss";
 
 const PageContent = (): JSX.Element => {
@@ -10,6 +11,7 @@ const PageContent = (): JSX.Element => {
       <About />
       <ProgrammingSkills />
       <GeneralSkills />
+      <Projects />
     </div>
   );
 };
