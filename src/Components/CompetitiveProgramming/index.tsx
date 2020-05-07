e2eb4c5e6afc937237a7dea2b16a7124d056dc3f
Ubiratan Neto <ubiratanneto37@hotmail.com>
2020-05-07 20:16:01 -0300

    fix typo on competitive-programming

--- src/Components/CompetitiveProgramming/index.tsx
+++ src/Components/CompetitiveProgramming/index.tsx
@@ -17,7 +17,7 @@ const CompetitiveProgramming = (): JSX.Element => {
       <p>
         Since 2016, I've been an active member of the competitive programming community, taking part
         on a large number of contests, regionally and globally, and achieving great results. I list
-        all my achievments on my LinkedIn profile. Even though I can no longer participate on most
+        all my achievements on my LinkedIn profile. Even though I can no longer participate on most
         of the competitions, due to several factors, I'm still active on some of the largest online
         contest platforms and communities. You can check my profile on each of them clicking on the
         links below:
